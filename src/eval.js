fc57613c26167a87d2699ecfa4211fd6c220efb7
Darren Siegel <siegel.darren@gmail.com>
2018-07-09 08:58:37 -0400

    robustness changes

--- src/eval.js
+++ src/eval.js
@@ -2,18 +2,25 @@ const { VM } = require('vm2');
 const { em } = require('./em');
 
 const vm = new VM({
-  timeout: 1000,
+  timeout: 300,
   sandbox: { em }
 });
 
 function run(expression) {
-  return vm.run(expression, 'vm.js');
+  try {
+    return vm.run(expression, 'vm.js');
+  } catch (e) {
+    return null;
+  }
 }
 
 function replaceVars(expression, evaluated) {
   
   Object.keys(evaluated).forEach(key => {
     const value = evaluated[key];
+    
+    // Handle double @ and single @
+    expression = expression.replace(new RegExp('@@' + key + '@@', 'g'), value);
     expression = expression.replace(new RegExp('@' + key + '@', 'g'), value);
   });
 
@@ -45,15 +52,20 @@ function evaluate(variables) {
       const varsReplaced = replaceVars(v.expression, evaluated);
 
       const evaled = run(varsReplaced);
-
-      evaluated[v.variable] = v.expression.startsWith('\"') && v.expression.endsWith('\"')
+      
+      if (evaled !== null) {
+        evaluated[v.variable] = v.expression.startsWith('\"') && v.expression.endsWith('\"')
         ? '\"' + evaled + '\"'
         : evaled;
+      } else {
+        evaluated[v.variable] = null;
+      }      
 
       return evaluated;
     }, {});
 
-  return Object.keys(evaluated).map(k => ({ variable: k, result: evaluated[k] }));
+  return Object.keys(evaluated).map(k => 
+    ({ variable: k, result: evaluated[k], errored: evaluated[k] === null }));
 } 
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -1,24 +1,28 @@
 
 const bodyParser = require("body-parser");
 const express = require("express");
+const cluster = require("express-cluster");
 const url = require("url");
 const util = require("util");
 
 const { evaluate } = require('./eval');
 
-const app = express();
-const router = express.Router();
+cluster(function(worker) {
+  
+  const app = express();
+  const router = express.Router();
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
+  app.use(bodyParser.json()); 
+  app.use(bodyParser.urlencoded({ extended: true })); 
 
-router.post('/sandbox', function(req, res) {
-  console.log(req.body);
-  res.send(evaluate(req.body.vars));
+  router.post('/sandbox', function(req, res) {
+    console.log(req.body);
+    res.send(evaluate(req.body.vars));
+  });
+
+  app.use('/', router);
+
+  return app.listen(8000, '0.0.0.0', function(){
+    console.log("evaluator running on port 8000");
+  });
 });
-
-app.use('/', router);
-
-app.listen(8000,function(){
-  console.log("sandbox running on port 8000");
-})
